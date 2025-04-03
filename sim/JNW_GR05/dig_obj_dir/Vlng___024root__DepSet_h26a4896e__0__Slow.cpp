a613b0772fed056e7e71051ee69b261f30464c90
Emilien15 <emilien88.henry@gmail.com>
2025-04-03 12:44:58 +0200

    simulation folder

---
+++ sim/JNW_GR05/dig_obj_dir/Vlng___024root__DepSet_h26a4896e__0__Slow.cpp
@@ -0,0 +1,156 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vlng.h for the primary calling header
+
+#include "Vlng__pch.h"
+#include "Vlng___024root.h"
+
+VL_ATTR_COLD void Vlng___024root___eval_static(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___eval_static\n"); );
+}
+
+VL_ATTR_COLD void Vlng___024root___eval_initial(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___eval_initial\n"); );
+    // Body
+    vlSelf->__Vtrigprevexpr___TOP__clk__0 = vlSelf->clk;
+}
+
+VL_ATTR_COLD void Vlng___024root___eval_final(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___eval_final\n"); );
+}
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vlng___024root___dump_triggers__stl(Vlng___024root* vlSelf);
+#endif  // VL_DEBUG
+VL_ATTR_COLD bool Vlng___024root___eval_phase__stl(Vlng___024root* vlSelf);
+
+VL_ATTR_COLD void Vlng___024root___eval_settle(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___eval_settle\n"); );
+    // Init
+    IData/*31:0*/ __VstlIterCount;
+    CData/*0:0*/ __VstlContinue;
+    // Body
+    __VstlIterCount = 0U;
+    vlSelf->__VstlFirstIteration = 1U;
+    __VstlContinue = 1U;
+    while (__VstlContinue) {
+        if (VL_UNLIKELY((0x64U < __VstlIterCount))) {
+#ifdef VL_DEBUG
+            Vlng___024root___dump_triggers__stl(vlSelf);
+#endif
+            VL_FATAL_MT("../../rtl/dig.v", 1, "", "Settle region did not converge.");
+        }
+        __VstlIterCount = ((IData)(1U) + __VstlIterCount);
+        __VstlContinue = 0U;
+        if (Vlng___024root___eval_phase__stl(vlSelf)) {
+            __VstlContinue = 1U;
+        }
+        vlSelf->__VstlFirstIteration = 0U;
+    }
+}
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vlng___024root___dump_triggers__stl(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___dump_triggers__stl\n"); );
+    // Body
+    if ((1U & (~ (IData)(vlSelf->__VstlTriggered.any())))) {
+        VL_DBG_MSGF("         No triggers active\n");
+    }
+    if ((1ULL & vlSelf->__VstlTriggered.word(0U))) {
+        VL_DBG_MSGF("         'stl' region trigger index 0 is active: Internal 'stl' trigger - first iteration\n");
+    }
+}
+#endif  // VL_DEBUG
+
+VL_ATTR_COLD void Vlng___024root___stl_sequent__TOP__0(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___stl_sequent__TOP__0\n"); );
+    // Body
+    vlSelf->temperature = (0xffU & ((IData)(0x14U) 
+                                    + ((IData)(0xfffffffaU) 
+                                       * ((IData)(vlSelf->temp_capture__DOT__captured_value) 
+                                          - (IData)(0x41U)))));
+}
+
+VL_ATTR_COLD void Vlng___024root___eval_stl(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___eval_stl\n"); );
+    // Body
+    if ((1ULL & vlSelf->__VstlTriggered.word(0U))) {
+        Vlng___024root___stl_sequent__TOP__0(vlSelf);
+    }
+}
+
+VL_ATTR_COLD void Vlng___024root___eval_triggers__stl(Vlng___024root* vlSelf);
+
+VL_ATTR_COLD bool Vlng___024root___eval_phase__stl(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___eval_phase__stl\n"); );
+    // Init
+    CData/*0:0*/ __VstlExecute;
+    // Body
+    Vlng___024root___eval_triggers__stl(vlSelf);
+    __VstlExecute = vlSelf->__VstlTriggered.any();
+    if (__VstlExecute) {
+        Vlng___024root___eval_stl(vlSelf);
+    }
+    return (__VstlExecute);
+}
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vlng___024root___dump_triggers__act(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___dump_triggers__act\n"); );
+    // Body
+    if ((1U & (~ (IData)(vlSelf->__VactTriggered.any())))) {
+        VL_DBG_MSGF("         No triggers active\n");
+    }
+    if ((1ULL & vlSelf->__VactTriggered.word(0U))) {
+        VL_DBG_MSGF("         'act' region trigger index 0 is active: @(posedge clk)\n");
+    }
+}
+#endif  // VL_DEBUG
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void Vlng___024root___dump_triggers__nba(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___dump_triggers__nba\n"); );
+    // Body
+    if ((1U & (~ (IData)(vlSelf->__VnbaTriggered.any())))) {
+        VL_DBG_MSGF("         No triggers active\n");
+    }
+    if ((1ULL & vlSelf->__VnbaTriggered.word(0U))) {
+        VL_DBG_MSGF("         'nba' region trigger index 0 is active: @(posedge clk)\n");
+    }
+}
+#endif  // VL_DEBUG
+
+VL_ATTR_COLD void Vlng___024root___ctor_var_reset(Vlng___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vlng__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vlng___024root___ctor_var_reset\n"); );
+    // Body
+    vlSelf->clk = VL_RAND_RESET_I(1);
+    vlSelf->pins = VL_RAND_RESET_I(8);
+    vlSelf->reset = VL_RAND_RESET_I(1);
+    vlSelf->temperature = VL_RAND_RESET_I(8);
+    vlSelf->temp_capture__DOT__counter = VL_RAND_RESET_I(20);
+    vlSelf->temp_capture__DOT__captured_value = VL_RAND_RESET_I(8);
+    vlSelf->temp_capture__DOT__state = VL_RAND_RESET_I(2);
+    vlSelf->__Vtrigprevexpr___TOP__clk__0 = VL_RAND_RESET_I(1);
+}
