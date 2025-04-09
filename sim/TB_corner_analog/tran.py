b73ade134a0f9cecd377ebef42a1cdf9c2fa1703
justlikemanuel <buchfink_manuel@web.de>
2025-04-09 16:49:08 +0200

    LVS success for OTA

--- sim/TB_corner_analog/tran.py
+++ sim/TB_corner_analog/tran.py
@@ -5,7 +5,53 @@ import os
 
 def main(directory="output_tran"):  # Directory containing the YAML files
     # List all YAML files in the directory
-    yaml_files = [f for f in os.listdir(directory) if f.endswith(".yaml")]
+    # yaml_files = [f for f in os.listdir(directory) if f.endswith(".yaml")]
+
+
+    # corners (without MC mismatch)
+    """
+    output_tran/tran_SchGtKssThVl
+    output_tran/tran_SchGtKffThVl
+    output_tran/tran_SchGtKsfThVl
+    output_tran/tran_SchGtKfsThVl
+    output_tran/tran_SchGtKssTlVl
+    output_tran/tran_SchGtKffTlVl
+    output_tran/tran_SchGtKsfTlVl
+    output_tran/tran_SchGtKfsTlVl
+    output_tran/tran_SchGtKssThVh
+    output_tran/tran_SchGtKffThVh
+    output_tran/tran_SchGtKsfThVh
+    output_tran/tran_SchGtKfsThVh
+    output_tran/tran_SchGtKssTlVh
+    output_tran/tran_SchGtKffTlVh
+    output_tran/tran_SchGtKsfTlVh
+    output_tran/tran_SchGtKfsTlVh"
+    """
+
+    yaml_names = [
+        "tran_SchGtKssThVl",
+        "tran_SchGtKffThVl",
+        "tran_SchGtKsfThVl",
+        "tran_SchGtKfsThVl",
+        "tran_SchGtKssTlVl",
+        "tran_SchGtKffTlVl",
+        "tran_SchGtKsfTlVl",
+        "tran_SchGtKfsTlVl",
+        "tran_SchGtKssThVh",
+        "tran_SchGtKffThVh",
+        "tran_SchGtKsfThVh",
+        "tran_SchGtKfsThVh",
+        "tran_SchGtKssTlVh",
+        "tran_SchGtKffTlVh",
+        "tran_SchGtKsfTlVh",
+        "tran_SchGtKfsTlVh"
+    ]
+
+    #   try to find the yaml files in the directory
+    yaml_files = []
+    for yaml_name in yaml_names:
+        yaml_files += [f for f in os.listdir(directory) if f.startswith(yaml_name) and f.endswith(".yaml")]
+
 
     print("Number of YAML files found:", len(yaml_files))
     print("Files found:", yaml_files)
@@ -33,6 +79,9 @@ def main(directory="output_tran"):  # Directory containing the YAML files
 
 
         # Extract temperatures (keys of the data, like i_out_-10, v_ref_0)
+       # print("Keys in the YAML file:", data.keys())
+        print("-----------------------------------")
+
         for key in data:
             if key.startswith(current_string):
                 temp = int(key.split("_")[-1])  # Extract temperature from the key
@@ -48,6 +97,13 @@ def main(directory="output_tran"):  # Directory containing the YAML files
 
         # convert the current from A to uA
         current_buffer = np.array(current_buffer) * 1e6
+
+        # add 0.1°C * corner_number to temperature 
+        temperatures = np.array(temperatures) + 0.1 * corner_number
+        # sort temperature and apply these indices also to current and voltage
+        temperatures, current_buffer, voltage_buffer = zip(*sorted(zip(temperatures, current_buffer, voltage_buffer)))
+
+        print("length of temperatures: ", len(temperatures), "length of current_buffer: ", len(current_buffer), "length of voltage_buffer: ", len(voltage_buffer))
 
         # plot with markers "x" and dashed line
         # fig1: current
