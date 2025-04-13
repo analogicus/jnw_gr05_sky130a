7dc6e16e1b8ef3c8f881ff5839d9dc9f7bb9a759
Emilien15 <emilien88.henry@gmail.com>
2025-04-13 23:44:26 +0200

    verilog code

---
+++ rtl/dig.v
@@ -0,0 +1,27 @@
+module dig (
+    input wire clk,
+    input wire Voutc,
+    output logic [7:0] T
+);
+
+    logic rst = 0;
+    logic [14:0] cycle_reset = 0;
+
+    always_ff @(posedge clk) begin
+        if (cycle_reset == 5906) begin
+            rst <= 1;
+            cycle_reset <= 0;
+        end else begin
+            rst <= 0;
+            cycle_reset <= cycle_reset + 1;
+        end
+    end
+
+    always_ff @(posedge clk) begin
+        if (rst)
+            T <= 0;
+        else if (Voutc)
+            T <= T + 1;
+    end
+
+endmodule
